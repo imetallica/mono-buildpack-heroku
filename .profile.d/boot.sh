8968bccab27aa426ce6e4e305c9f33dc84cffc1e
Iuri L. Machado <iuri.machado@linuxmail.org>
2016-07-12 21:11:10 -0300

    Create boot.sh

---
+++ .profile.d/boot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin" 
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
+export PKG_CONFIG_PATH="/app/mono/lib/pkgconfig"
